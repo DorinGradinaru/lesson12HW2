333ee996392ac10b5d4409e2a045ef36671c438b
DorinGradinaru <dorin.gradinaru@icloud.com>
2024-03-05 11:49:23 +0200

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+// var testVariable = 7
+// const str = "text"
+// let bool = false
+// let unde = undefined 
+// let testNull = null 
+// let testBigInt = BigInt(7777) 
+// let symb = Symbol('') 
+
+
+// console.log(testVariable)
+// console.log(str)
+// console.log(bool)
+// console.log(unde)
+// console.log(testNull)
+// console.log(testBigInt)
+// console.log(symb)
+
+
+let num1 = 4
+let num2 = 2
+let bool = true
+let num3 = '5'
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
+console.log(num1 ** num2)
+console.log(++ num1)
+console.log(-- num1)
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 <= num2)
+console.log(num1 >= num2)
+console.log(!bool)
+console.log(num1 == num2)
+console.log(num1 === num3)
